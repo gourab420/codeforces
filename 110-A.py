d72e7f8a2a83733430c59bc5b33991409143fec3
gourab420 <63583812+gourab420@users.noreply.github.com>
2025-09-07 12:28:47 +0600

    Add files via upload

---
+++ 110-A.py
@@ -0,0 +1,31 @@
+n=input()
+r=n.count('7') + n.count('4')
+if  r==4 or r==7:
+    print('YES')
+else:
+    print('NO')
+
+
+
+
+
+
+
+
+
+
+# for i in range(len(n)):
+#     if int(n[i]) == 4 or int(n[i]) == 7:
+#         c+=1
+#
+# if c==4 or c==7:
+#     print("YES")
+# else:
+#     print("NO")
+
+
+# if c==len(n):
+#     if '4' in n and '7' in n:
+#         print("YES")
+#     else:
+#         print("NO")
